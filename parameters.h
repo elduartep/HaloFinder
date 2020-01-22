9e7f926e17f124cfb741dbd0fdd51ffdcd1c5178
elduartep <elduartep@gmail.com>
2020-01-22 22:28:21 +0100

    Add files via upload

---
+++ parameters.h
@@ -0,0 +1,9 @@
+const double Om=0.311051;
+const double Ol=0.688870;
+const double H0=67.66;
+
+
+const double Lbox = 512.;		//	longitud de la caja en [Mpc/h]
+const int    np   = 512;		//	numero de particulas en cada dimension
+
+char dark_matter_file[]="matter.txt";		//	is the dark matter catalog file
